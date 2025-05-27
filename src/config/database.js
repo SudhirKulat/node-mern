c455c2aea666e381d1dff08cd20de82b0e263549
Sudhir <31650084+SudhirKulat@users.noreply.github.com>
2025-05-27 12:59:45 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://sudhirkulat:7Vr6lL8WZ0IauuDj@sudhircodeman.qfb6p.mongodb.net/devTinder"
+    "MONGO_URL"
   );
 };
 
